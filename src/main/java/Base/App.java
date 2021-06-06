f3cb972381855f54546de1e51f269bad65cbf82b
Alek Dussuau <ajdussuau@gmail.com>
2021-06-06 19:42:46 -0400

    Initial Solution

--- src/main/java/Base/App.java
+++ src/main/java/Base/App.java
@@ -2,6 +2,8 @@ package Base;
 
 import java.util.Scanner;
 
+import static java.lang.Math.pow;
+
 /*
 UCF COP3330 Summer 2021 Assignment 1 Solution
 Copyright 2021 Alek Dussuau
@@ -20,7 +22,7 @@ The formula you’ll use for this is A = P(1 + r/n)^(n*t) where
     *t is the number of years the amount is invested.
     *n is the number of times the interest is compounded per year.
     *A is the amount at the end of the investment.
-    
+
 Example Output
 What is the principal amount? 1500
 What is the rate? 4.3
@@ -42,5 +44,31 @@ Implement this program as a GUI app that automatically updates the values when a
 public class App {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+
+        //inputs
+        System.out.print("Enter the principal: ");
+        String buff_principal = input.nextLine();
+        System.out.print("Enter the rate of interest: ");
+        String buff_interest = input.nextLine();
+        System.out.print("Enter the number of years: ");
+        String buff_years = input.nextLine();
+        System.out.print("What is the number of times the interest is compounded per year? ");
+        String buff_compounded = input.nextLine();
+
+        //parsing
+        int principal = Integer.parseInt(buff_principal);
+        double interest = Double.parseDouble(buff_interest);
+        int years = Integer.parseInt(buff_years);
+        int compounded = Integer.parseInt(buff_compounded);
+
+        //math
+        double rate = interest/100;
+        double a = 1+(rate/compounded);
+        double b = compounded*years;
+        double c = pow(a,b);
+        double amount = principal*c;
+
+        System.out.printf("$%d invested at %.1f%% for %d years compounded %d times per year is $%.2f",
+                            principal,interest,years,compounded,amount);
     }
 }
